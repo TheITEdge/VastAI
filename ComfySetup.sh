88df9d5ae6a97559415cdd80dcdd584c6e00755f
Steve <161262583+TheITEdge@users.noreply.github.com>
2024-02-26 12:32:36 +1000

    Update ComfySetup.sh

--- ComfySetup.sh
+++ ComfySetup.sh
@@ -1,3 +1,6 @@
+cd /workspace
+python -s -m pip install segment_anything omegaconf
+
 
 cd storage/stable_diffusion/models/ckpt
 wget https://huggingface.co/lllyasviel/fav_models/resolve/main/fav/realisticVisionV51_v51VAE.safetensors
