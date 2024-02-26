b46aec9c579457fb3802f6c7afcfdde003ea8884
Steve <161262583+TheITEdge@users.noreply.github.com>
2024-02-26 12:36:10 +1000

    Update ComfySetup.sh

--- ComfySetup.sh
+++ ComfySetup.sh
@@ -6,6 +6,13 @@ cd storage/stable_diffusion/models/ckpt
 wget https://huggingface.co/lllyasviel/fav_models/resolve/main/fav/realisticVisionV51_v51VAE.safetensors
 
 
+cd /opt/ComfyUI/models/upscale_models/
+wget https://huggingface.co/gemasai/4x_NMKD-Superscale-SP_178000_G/raw/main/4x_NMKD-Superscale-SP_178000_G.pth
+cd ../controlnet/
+wget https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_depth-fp16.safetensors
+
+comfyui manager install all
+
 
 
 
