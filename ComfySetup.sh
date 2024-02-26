45f3ae63ba339b3a2445cb7b88b5096635a15c82
Steve <161262583+TheITEdge@users.noreply.github.com>
2024-02-26 12:27:59 +1000

    Update ComfySetup.sh

--- ComfySetup.sh
+++ ComfySetup.sh
@@ -1,7 +1,10 @@
 
+cd storage/stable_diffusion/models/ckpt
+wget https://huggingface.co/lllyasviel/fav_models/resolve/main/fav/realisticVisionV51_v51VAE.safetensors
 
 
-https://huggingface.co/lllyasviel/fav_models/raw/main/fav/realisticVisionV51_v51VAE.safetensors
+
+
 
 
 
